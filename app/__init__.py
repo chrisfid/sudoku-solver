8d39e3f213958d248f6adf9381d22aee3c808b83
chrisfid <krzysztof.fidyka@gmail.com>
2021-07-08 11:02:31 +0200

    fix: .env file; update requirements

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,14 @@ from app.solve import SudokuSolver
 from flask import Flask
 from app.solve import SudokuSolver
 import os
+from dotenv import load_dotenv
 
-
+load_dotenv()
 app = Flask(__name__)
 solver = SudokuSolver()
 
-app.config['SECRET_KEY'] = 'WuAIg9VzjDlAopVxSK9xfQ'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
 from app import routes
 
-app.run(debug=1)
+app.run(debug=os.environ.get('DEBUG'))
