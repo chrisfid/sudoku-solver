87fd3ac202b3e0be4618d742adff3a353ee05fe0
chrisfid <krzysztof.fidyka@gmail.com>
2021-07-09 20:46:37 +0200

    feat: add sudoku levels

--- app/routes.py
+++ app/routes.py
@@ -7,17 +7,26 @@ from app import app, solver
 @app.route("/", methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        # Reset
-        if request.form['sudoku'] == 'Reset':
-            session['board'] = solver.create_empty_board()
-        # Solve
-        elif request.form['sudoku'] == 'Solve':
-            cells = request.form.getlist('cells', type=int)
-            board = solver.multidict_to_board(cells)
-            session['board'] = solver.solve(board)
-        # Randomize
-        elif request.form['sudoku'] == 'Randomize':
-            session['board'] = solver.new_board(level=1)
+        if 'sudoku-option' in request.form:
+            # Reset
+            if request.form['sudoku-option'] == 'Reset':
+                session['board'] = solver.create_empty_board()
+            # Solve
+            elif request.form['sudoku-option'] == 'Solve':
+                cells = request.form.getlist('cells', type=int)
+                board = solver.multidict_to_board(cells)
+                session['board'] = solver.solve(board)
+            # Randomize
+            elif request.form['sudoku-option'] == 'Randomize':
+                session['board'] = solver.new_board(
+                    level=session['level'] if session['level'] is not None else 1)
+        elif 'sudoku-level' in request.form:
+            if request.form['sudoku-level'] == 'Easy':
+                session['level'] = 1
+            elif request.form['sudoku-level'] == 'Medium':
+                session['level'] = 2
+            elif request.form['sudoku-level'] == 'Hard':
+                session['level'] = 3
         return redirect(url_for('index'))
 
     if request.method == 'GET':
--- app/solve.py
+++ app/solve.py
@@ -82,6 +82,7 @@ class SudokuSolver:
 
     def new_board(self, level):
         url = f'http://www.cs.utep.edu/cheon/ws/sudoku/new/?size=9&level={level}'
+        print(url)
 
         resp = requests.get(url)
         data = resp.json()
